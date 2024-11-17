205f5e855689990e2ff1ff093dc84532243e0bd4
Alejo-P <marcelo.pinzon@epn.edu.ec>
2024-11-16 19:44:06 -0500

    Actualizar la documentación de la API y ajustar la carga del archivo de Swagger

--- src/server.js
+++ src/server.js
@@ -4,7 +4,8 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import swaggerUi from 'swagger-ui-express';
-import swaggerJSDoc from 'swagger-jsdoc';
+import fs from 'fs';
+import path from 'path';
 
 import EmpleadosRoutes from './routers/EmpleadosRoutes.js';
 import AsistenciasRoutes from './routers/AsistenciasRoutes.js';
@@ -12,27 +13,6 @@ import PagosRoutes from './routers/PagosRoutes.js';
 import ClientesRoutes from './routers/ClientesRoutes.js';
 import VehiculosRoutes from './routers/VehiculosRoutes.js';
 import MantenimientosRoutes from './routers/MantenimientosRoutes.js';
-
-const swaggerOptions = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Full Prestige - API',
-            version: '1.0.0',
-            description: 'API para la gestión de un taller automotriz',
-            contact: {
-                name: 'Marcelo Pinzón',
-                email: 'marcelo.pinzon@epn.edu.ec',
-            },
-        },
-        servers: [
-            {
-                url: 'http://localhost:3000/api/v1',
-            },
-        ],
-    },
-    apis: ['./src/routers/*.js'],
-};
 
 dotenv.config();
 const app = express();
@@ -55,8 +35,13 @@ app.use('/api/v1', ClientesRoutes);
 app.use('/api/v1', VehiculosRoutes);
 app.use('/api/v1', MantenimientosRoutes);
 
-// Ruta para la documentación
-app.use('/api/v1/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJSDoc(swaggerOptions)));
+// Documentación de la API
+const swaggerDocument = JSON.parse(
+    fs.readFileSync(path.resolve('./API-Documentation.json'), 'utf8')
+);
+
+// Ruta para la documentación de Swagger UI
+app.use('/api/v1/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Manejo de una ruta que no sea encontrada
 app.use((req,res)=>res.status(404).send("Endpoint no encontrado - 404"))
