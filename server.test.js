12fe1e31670aa664570a17a609b67b19fa3a117f
Alejo-P <marcelo.pinzon@epn.edu.ec>
2024-11-29 08:46:36 -0500

    Agregar archivo env.js al .gitignore y crear prueba para registro de empleados

--- server.test.js
+++ server.test.js
@@ -1,6 +1,7 @@
 import request from "supertest";
 import app from "./src/server.js";
 import { connect, disconnect } from "./src/database.js";
+import {ej_empleado} from "./env.js";
 
 beforeAll(async () => {
     // Conectar a la base de datos en memoria antes de ejecutar las pruebas
@@ -11,6 +12,17 @@ describe("GET /", () => {
     it("should return 200 OK", async () => {
         const response = await request(app).get("/");
         expect(response.status).toBe(200);
+    });
+});
+
+/*-------------------------#Rutas para empleados#-------------------------*/
+
+describe("POST /api/v1/register", () => {
+    it("should return 201 Created", async () => {
+        const response = await request(app)
+            .post("/api/v1/register")
+            .send(ej_empleado);
+        expect(response.status).toBe(201);
     });
 });
 
