0d939f7f6fdd4d3c05e056bab1a45226a989f668
Alejo-P <marcelo.pinzon@epn.edu.ec>
2024-11-29 10:00:57 -0500

    Actualizar .gitignore para excluir tests/env.js; modificar pruebas para usar datos de usuario desde el nuevo archivo de entorno y agregar pruebas para obtener empleados.

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,7 +1,10 @@
 import request from "supertest";
 import app from "../src/server.js";
 import { connect, disconnect } from "../src/database.js";
-import {ej_empleado} from "../env.js";
+import {
+    ej_admin as adminUser,
+    ej_empleado as empleadoUser,
+} from "./env.js";
 
 beforeAll(async () => {
     // Conectar a la base de datos en memoria antes de ejecutar las pruebas
@@ -23,11 +26,21 @@ describe("GET /", () => {
 let token = "";
 
 /*-------------------------#Rutas para empleados#-------------------------*/
-describe("POST /api/v1/register", () => {
+describe("POST /api/v1/register - Administrador", () => {
     it("should return 201 Created", async () => {
         const response = await request(app)
             .post("/api/v1/register")
-            .send(ej_empleado);
+            .send(adminUser);
+
+        expect(response.status).toBe(201);
+    });
+}, 5000);
+
+describe("POST /api/v1/register - Empleado", () => {
+    it("should return 201 Created", async () => {
+        const response = await request(app)
+            .post("/api/v1/register")
+            .send(empleadoUser);
 
         expect(response.status).toBe(201);
     });
@@ -37,9 +50,32 @@ describe("POST /api/v1/login", () => {
     it("should return 200 OK", async () => {
         const response = await request(app)
             .post("/api/v1/login")
-            .send(ej_empleado);
+            .send(adminUser);
         
-        token = response.body.token;
+        token = response.body.empleado.token;
         expect(response.status).toBe(200);
     });
 }, 5000);
+
+describe("GET /api/v1/employees", () => {
+    it("should return 200 OK", async () => {
+        const response = await request(app)
+            .get("/api/v1/employees")
+            .set("Authorization", `Bearer ${token}`);
+        
+        console.log(response.body);
+        expect(response.status).toBe(200);
+    });
+});
+
+describe("GET /api/v1/employee/:cedula", () => {
+    it("should return 200 OK", async () => {
+        const response = await request(app)
+            .get(`/api/v1/employee/${ej_empleado.cedula}`)
+            .set("Authorization", `Bearer ${token}`);
+        
+        expect(response.status).toBe(200);
+    });
+});
+
+
